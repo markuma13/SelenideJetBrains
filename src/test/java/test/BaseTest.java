fb7ed39e05de055e3f291fca3626db259c5cc895
markuma13 <markumanskiy@gmail.com>
2023-11-09 16:57:37 +0500

    updateTest

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -4,12 +4,9 @@ import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
